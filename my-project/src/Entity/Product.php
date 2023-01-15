4066379ef92396def3a86443ed5f432740a8f3d0
andrii <andrii.sukhoi@gmail.com>
2023-01-15 17:23:26 +0100

    Add ProductController

--- my-project/src/Entity/Product.php
+++ my-project/src/Entity/Product.php
@@ -4,8 +4,11 @@ namespace App\Entity;
 
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[HasLifecycleCallbacks]
 class Product
 {
     #[ORM\Id]
@@ -20,6 +23,7 @@ class Product
     private ?\DateTimeImmutable $created_at = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $description = null;
     
     #[ORM\Column]
@@ -64,5 +68,23 @@ class Product
         $this->description = $description;
 
         return $this;
+    }
+    
+    public function setPrice(int $price)
+    {
+        $this->price = $price;
+        
+        return $this;
+    }
+    
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+    
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): void
+    {
+        $this->created_at = new \DateTimeImmutable();
     }
 }
